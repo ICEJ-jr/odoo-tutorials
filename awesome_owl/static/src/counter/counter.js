49e682ebe13baaea1979679edc71c881784516d5
Isaac Johnson <40847491+ICEJ-jr@users.noreply.github.com>
2024-10-30 12:39:37 +0000

    1.6: the sum of two counters

--- awesome_owl/static/src/counter/counter.js
+++ awesome_owl/static/src/counter/counter.js
@@ -5,11 +5,5 @@ import { Component, useState } from '@odoo/owl';
 export class Counter extends Component {
   static template = 'awesome_owl.counter';
 
-  setup() {
-    this.state = useState({ value: 0 });
-  }
-
-  increment() {
-    this.state.value++;
-  }
+  static props = ['value', 'increment?'];
 }
--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,23 +1,15 @@
 /** @odoo-module **/
 
-import { Component, markup } from '@odoo/owl';
-import { Card } from "./card/card";
+import { Component, markup, useState } from '@odoo/owl';
+import { Counter } from './counter/counter';
 
 export class Playground extends Component {
   static template = 'awesome_owl.playground';
 
-  static components = { Card };
+  static components = { Counter };
 
-  cards = [
-    {
-      title: 'Card 1',
-      content:
-        "<div class='text-primary'>This is a simple card with some content.</div>"
-      ,
-    },
-    {
-      title: 'Card 2',
-      content:  markup("<div class='text-primary'>This is another simple card with content.</div>"),
-    },
-  ];
+  setup() {
+    this.state = useState({ valueA: 0, valueB: 0});
+  }
+
 }
